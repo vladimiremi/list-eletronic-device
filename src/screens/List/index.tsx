3b1ce7c25f57b2ab391977791c86432443ffd44a
Vladimir Costa <41305527+vladimiremi@users.noreply.github.com>
2023-04-26 20:05:50 -0300

    refactor: use Header

--- src/screens/List/index.tsx
+++ src/screens/List/index.tsx
@@ -1,17 +1,24 @@
-import { FlatList, Text } from 'react-native'
-import { Container, Separator } from './styles'
+import { FlatList } from 'react-native'
+import { Container } from './styles'
 import { ItemDevice } from '../../components/ItemDevice'
 import { devicesMock } from '../../data/devices'
 import Divider from '../../components/Divider'
+import { Header } from '../../components/Header'
 
 export function List() {
   return (
     <Container>
+      <Divider top={22} />
+
+      <Header title="Lista de dispositivos" />
+
+      <Divider top={10} />
+
       <FlatList
         showsVerticalScrollIndicator={false}
         data={devicesMock}
         keyExtractor={(_, index) => String(index)}
-        ItemSeparatorComponent={() => <Divider top={11} />}
+        ItemSeparatorComponent={() => <Divider top={16} />}
         renderItem={({ item, index }) => {
           return <ItemDevice data={item} key={index} />
         }}
