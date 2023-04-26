d8a10c8e0873e8a86e447e25f7573dff5846aa64
Vladimir Costa <41305527+vladimiremi@users.noreply.github.com>
2023-04-25 21:37:19 -0300

    chore: add fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,24 @@
+import {
+  Overpass_700Bold,
+  Overpass_600SemiBold,
+  Overpass_400Regular,
+  Overpass_300Light,
+  useFonts,
+} from '@expo-google-fonts/overpass';
 import { ThemeProvider } from "styled-components";
 import { List } from "./src/screens/List";
 import theme from "./src/theme";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Overpass_700Bold,
+    Overpass_600SemiBold,
+    Overpass_400Regular,
+    Overpass_300Light,
+  });
   return (
     <ThemeProvider theme={theme}>
-      <List />
+      {fontsLoaded ? <List /> : false}
     </ThemeProvider>
   );
 }
