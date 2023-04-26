5d06626a9f723f6cd6b7d1c485726a8c74f945ca
Vladimir Costa <41305527+vladimiremi@users.noreply.github.com>
2023-04-26 20:04:45 -0300

    refactor: config status bar

--- App.tsx
+++ App.tsx
@@ -4,10 +4,11 @@ import {
   Overpass_400Regular,
   Overpass_300Light,
   useFonts,
-} from '@expo-google-fonts/overpass';
-import { ThemeProvider } from "styled-components/native";
-import { List } from "./src/screens/List";
-import theme from "./src/theme";
+} from '@expo-google-fonts/overpass'
+import { ThemeProvider } from 'styled-components/native'
+import { List } from './src/screens/List'
+import theme from './src/theme'
+import { StatusBar } from 'react-native'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -15,11 +16,15 @@ export default function App() {
     Overpass_600SemiBold,
     Overpass_400Regular,
     Overpass_300Light,
-  });
+  })
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       {fontsLoaded ? <List /> : false}
     </ThemeProvider>
-  );
+  )
 }
-
