81895ec197984f7d1719805f07a30782d226e8ba
Vladimir Costa <41305527+vladimiremi@users.noreply.github.com>
2023-04-25 21:44:28 -0300

    feat: add default text component

---
+++ src/components/Text/index.tsx
@@ -0,0 +1,32 @@
+import { TextProps } from 'react-native';
+import {TextContainer} from './styles';
+import { ReactNode } from 'react';
+
+export interface IText extends TextProps {
+  children: ReactNode;
+  fontSize: number;
+  fontFamily: string;
+  color: string;
+  textAlign?: 'center' | 'right' | 'left';
+}
+
+export const Text = ({
+  children,
+  fontSize,
+  fontFamily,
+  color,
+  textAlign = 'center',
+  ...rest
+}: IText) => {
+    return (
+      <TextContainer
+        fontFamily={fontFamily}
+        fontSize={fontSize}
+        color={color}
+        textAlign={textAlign}
+        {...rest}
+      >
+        {children}
+      </TextContainer>
+    );
+  };
