8fa2e2644798bbe18d542ee886fbb883e256fa1d
Vladimir Costa <41305527+vladimiremi@users.noreply.github.com>
2023-04-26 08:04:40 -0300

    improvement: render item devices with FlatList

--- src/screens/List/index.tsx
+++ src/screens/List/index.tsx
@@ -1,11 +1,21 @@
-import { Text } from 'react-native'
-import { Container } from './styles'
+import { FlatList, Text } from 'react-native'
+import { Container, Separator } from './styles'
 import { ItemDevice } from '../../components/ItemDevice'
+import { devicesMock } from '../../data/devices'
+import Divider from '../../components/Divider'
 
 export function List() {
   return (
     <Container>
-      <ItemDevice />
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        data={devicesMock}
+        keyExtractor={(_, index) => String(index)}
+        ItemSeparatorComponent={() => <Divider top={11} />}
+        renderItem={({ item, index }) => {
+          return <ItemDevice data={item} key={index} />
+        }}
+      />
     </Container>
   )
 }
--- src/screens/List/styles.ts
+++ src/screens/List/styles.ts
@@ -1,9 +1,7 @@
-import styled from "styled-components/native";
-
+import styled from 'styled-components/native'
 
 export const Container = styled.View`
-    background-color: white;
-    flex: 1;
-    align-items: center;
-    justify-content: center;
+  background-color: ${({ theme }) => theme.COLORS.DARK};
+  flex: 1;
+  align-items: center;
 `
