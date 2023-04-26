0690ba5877b55e553085dc54b4d18dcd2c990e46
Vladimir Costa <41305527+vladimiremi@users.noreply.github.com>
2023-04-26 08:00:21 -0300

    feat: add data mock

---
+++ src/data/devices.ts
@@ -0,0 +1,38 @@
+export const devicesMock = [
+  {
+    name: 'Camera 1',
+    serial: '123456789',
+    macAddress: '00:11:22:33:44:55',
+    type: 'camera',
+  },
+  {
+    name: 'Sensor 1',
+    serial: '987654321',
+    macAddress: 'AA:BB:CC:DD:EE:FF',
+    type: 'sensor',
+  },
+  {
+    name: 'Remote 1',
+    serial: '456789123',
+    macAddress: '11:22:33:44:55:66',
+    type: 'remote control',
+  },
+  {
+    name: 'Camera 2',
+    serial: '555666777',
+    macAddress: 'FF:EE:DD:CC:BB:AA',
+    type: 'camera',
+  },
+  {
+    name: 'Sensor 2',
+    serial: '111222333',
+    macAddress: '12:34:56:78:90:AB',
+    type: 'sensor',
+  },
+  {
+    name: 'Remote 2',
+    serial: '777888999',
+    macAddress: '99:88:77:66:55:44',
+    type: 'remote control',
+  },
+]
