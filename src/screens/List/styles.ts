25d2fbf543579ea19cdaec6b7c61cb9f374ddb29
Vladimir Costa <41305527+vladimiremi@users.noreply.github.com>
2023-04-26 20:08:14 -0300

    refactor: add padding at list

--- src/screens/List/styles.ts
+++ src/screens/List/styles.ts
@@ -3,5 +3,5 @@ import styled from 'styled-components/native'
 export const Container = styled.View`
   background-color: ${({ theme }) => theme.COLORS.DARK};
   flex: 1;
-  align-items: center;
+  padding: 15px 20px;
 `
